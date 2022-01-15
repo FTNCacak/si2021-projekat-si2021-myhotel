bbd836f459406dd0db9480d8eb84575be45038ca
zoran25 <269-2018@ftn.edu.rs>
2022-01-15 22:02:02 +0100

    Changed Database a bit and needed to fix some code. Finished code for Repository classes and getAll() method!

---
+++ My Hotel/DataLayer/ApartmentRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ApartmentRepository
+    {
+        private string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=MyHotel;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public List<Apartment> GetAllApartments()
+        {
+            List<Apartment> Apartments = new List<Apartment>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Apartment";
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Apartment Apartment = new Apartment();
+                    Apartment.Id = sqlDataReader.GetString(0);
+                    Apartment.Adress = sqlDataReader.GetString(1);
+                    Apartment.Type = sqlDataReader.GetString(2);
+                    Apartment.Price = sqlDataReader.GetDecimal(3);
+
+                    Apartments.Add(Apartment);
+                }
+
+                sqlConnection.Close();
+            }
+
+            return Apartments;
+        }
+
+
+    }
+}
